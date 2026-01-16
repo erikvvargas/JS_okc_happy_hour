233d69a5105bcb0ce2de3f6f5958979c368ebeb8
erikvvargas <erikvvargas@gmail.com>
2026-01-16 16:30:34 -0600

    updating main.py and requirements for Python

--- backend/main.py
+++ backend/main.py
@@ -12,11 +12,12 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173",
-        "http://192.168.1.180:5173"
-    ],  
+    # allow_origins=[
+    #     "http://localhost:5173", 
+    #     "http://127.0.0.1:5173",
+    #     "http://192.168.1.180:5173"
+    # ],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
