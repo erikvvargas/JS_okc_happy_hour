e7c3c3764afc5f24c23a6a3b51177557e343f040
erikvvargas <erikvvargas@gmail.com>
2026-01-17 15:02:47 -0600

    Updating correct regex for Vercel domain

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,8 @@ app.add_middleware(
     #     "http://127.0.0.1:5173",
     #     "http://192.168.1.180:5173"
     # ],  
-    allow_origins=[r"https://.*\.vercel\.app"],
+    allow_origins=["https://js-okc-happy-hour.vercel.app/"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
